5551101284a1739a9e91ed5f94fbf8d8d9b76aac
eranarktrade <69576887+eranarktrade@users.noreply.github.com>
2020-10-11 15:37:15 +0300

    Update w.sh

--- profile/w.sh
+++ profile/w.sh
@@ -1,1 +1,15 @@
+echo -e -n "\033[1;35m";
+BUILD_DIR=${1:-}
+CACHE_DIR=${2:-}
+ENV_DIR=${3:-}
 
+echo "$ENV_DIR"
+if [ -d "$ENV_DIR" ]; then
+    for e in $(ls $ENV_DIR); do    
+      echo "$e"
+      :
+    done
+  fi
+
+
+echo -e -n "\033[0m";
