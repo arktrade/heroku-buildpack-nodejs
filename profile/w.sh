ef066ad89d7b310d4dbb73adbd8ed212baa04662
eranarktrade <69576887+eranarktrade@users.noreply.github.com>
2020-10-11 15:51:32 +0300

    Update w.sh

--- profile/w.sh
+++ profile/w.sh
@@ -1,15 +1,4 @@
 
-BUILD_DIR=${1:-}
-#CACHE_DIR=${2:-}
-ENV_DIR=${3:-}
-
-echo "$ENV_DIR"
-if [ -d "$ENV_DIR" ]; then
-    for e in $(ls $ENV_DIR); do    
-      echo "$e"
-      :
-    done
-  fi
-
+echo $WRK
 
 
