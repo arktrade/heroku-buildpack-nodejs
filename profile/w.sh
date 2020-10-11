770eae3b9e71c7b8c20dd642fa8068c24d688018
eranarktrade <69576887+eranarktrade@users.noreply.github.com>
2020-10-11 11:41:59 +0300

    Update w.sh

--- profile/w.sh
+++ profile/w.sh
@@ -1,1 +1,15 @@
 
+BUILD_DIR=${1:-}
+
+if [ -d "$BUILD_DIR" ]; then
+    for e in $(ls $BUILD_DIR); do      
+      export "$e=$(cat $BUILD_DIR/$e)"    
+      :
+    done
+  fi
+
+echo -e -n "\033[1;36m";
+echo "RELEASE";
+echo "$BUILD_DIR";
+echo "$WRK";
+echo "$STAM";
